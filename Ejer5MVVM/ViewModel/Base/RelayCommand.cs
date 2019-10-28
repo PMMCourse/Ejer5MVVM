2a5d82c6a7732332df0c90130f5ca06170863aea
daniel.romero.rodriguez2@gmail.com <daniel.romero.rodriguez2@gmail.com>
2019-10-28 18:39:13 +0100

    nuevo commit

--- Ejer5MVVM/ViewModel/Base/RelayCommand.cs
+++ Ejer5MVVM/ViewModel/Base/RelayCommand.cs
@@ -7,7 +7,29 @@ using System.Threading.Tasks;
 
 namespace Ejer5MVVM.ViewModel.Base
 {
-        public class RelayCommand<T> : ICommand
+    public class RelayCommand : ICommand
+    {
+        private Action _action;
+
+        public RelayCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action?.Invoke();
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
         {
             public RelayCommand(Action<T> action)
             {
