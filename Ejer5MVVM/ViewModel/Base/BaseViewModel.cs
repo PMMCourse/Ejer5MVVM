53b097b7b609c851e8f56e820b4896b077b8cc9c
nacho190 <nacho190@gmail.com>
2019-10-29 19:29:15 +0100

    MVVM

---
+++ Ejer5MVVM/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer5MVVM.ViewModel.Base
+{
+        //Implementamos INotifyPropertyChanged para poder invocar el evento de PropertyChanged
+        //Que nos permitirá notificar a Vista y ViewModel por igual
+
+        //Está clase se plantea abstracta (Que no tiene instancia, ya que solo la usaremos como padre)
+        public abstract class BaseViewModel : INotifyPropertyChanged
+        {
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public void RaiseProperty([CallerMemberName] string propertyName = "")
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    
+}
---
+++ Ejer5MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,84 @@
+﻿using Ejer5MVVM.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Ejer5MVVM.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private RelayCommand _addname;
+        private RelayCommand _CheckIndice;
+        public ICommand CheckIndice => _CheckIndice;
+        public ICommand addname => _addname;
+        private int _indice;
+        public List<string> _Nombres = new List<string>()
+        {
+            "Ana",
+            "Nacho",     
+            "Pedro",
+        };
+
+        public MainViewModel()
+        {
+            _CheckIndice = new RelayCommand(Checkindice);
+            _addname = new RelayCommand(Addname);
+        }
+        private string _NombreSele;
+        public string NombreSele
+        {
+            get
+            {
+                return _NombreSele;
+            }
+            set
+            {
+               _NombreSele = value;
+                RaiseProperty();
+            }
+        }
+
+        public List<string> Nombres
+        {
+            get => _Nombres;
+            set
+            {
+                _Nombres = value;
+                RaiseProperty();
+            }
+        }
+      
+        private void Checkindice()
+        {
+            for (int i = 0; i < Nombres.Count; i++)
+            {
+                if (_indice == Nombres.IndexOf(Nombres[i]))
+                {
+                    MessageBox.Show(""+Nombres[i]+" "+ Nombres.IndexOf(Nombres[i]));
+                }
+            }
+        }
+
+        private void Addname()
+        {
+            _Nombres.Add("pepe");
+            MessageBox.Show("nombre añadido");            
+        }
+        public int indice
+        {
+            get =>_indice;
+            set
+            {
+                _indice = value;
+                RaiseProperty();
+            }
+        }
+    }
+}
