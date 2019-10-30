f7647b5fa8ac68e13046ed6326b0299ef245dd13
daniel.romero.rodriguez2@gmail.com <daniel.romero.rodriguez2@gmail.com>
2019-10-30 22:36:28 +0100

    codigo refactorizado y funcionando.

--- Ejer5MVVM/ViewModel/MainViewModel.cs
+++ Ejer5MVVM/ViewModel/MainViewModel.cs
@@ -11,50 +11,77 @@ namespace Ejer5MVVM.ViewModel
 {
     class MainViewModel : BaseViewModel
     {
+       /* private RelayCommand _checkTextGreetingsCommand;
+        public ICommand CheckTextGreetingsCommand => _checkTextGreetingsCommand;*/
+
         private RelayCommand<String> _insertNumberCommand;
         public ICommand InsertNumberCommand => _insertNumberCommand;
+
+        private RelayCommand<String> _insertOperator;
+        public ICommand InsertOperator => _insertOperator;
+
         public MainViewModel()
         {
+            //_checkTextGreetingsCommand = new RelayCommand(PerformInsertNumber);
             _insertNumberCommand = new RelayCommand<String>(PerformInsertNumber);
+            _insertOperator = new RelayCommand<String>(PerformOperator);
         }
-
-        private void PerformInsertNumber(String obj)
+        private void PerformOperator(String obj)
+        {
+            OperationResult += obj;
+        }
+        public void PerformInsertNumber(String obj)
         {
             Double numero1, numero2;
             String[] array = null;
-            if (obj.Contains("+")) {
-                array = obj.Split('+');
-                numero1 = Convert.ToDouble(array[0].Trim());
-                numero2 = Convert.ToDouble(array[1].Trim());
-                Double resultado = numero1 + numero2;
-                _numbersOperate = resultado.ToString();
-                MessageBox.Show(_numbersOperate);
-            } else if (obj.Contains("-")) {
-                array = obj.Split('-');
-                numero1 = Convert.ToDouble(array[0].Trim());
-                numero2 = Convert.ToDouble(array[1].Trim());
-                Double resultado = numero1 - numero2;
-                _numbersOperate = resultado.ToString();
-                MessageBox.Show(_numbersOperate);
+            if (OperationResult.Contains("+")) {
+                array = OperationResult.Split('+');
+                PerformOperationSum(array);
+            } else if (OperationResult.Contains("-")) {
+                array = OperationResult.Split('-');
+                PerformOperationDeduct(array);
             }
-            else if (obj.Contains("*"))
-            {
-                array = obj.Split('*');
-                numero1 = Convert.ToDouble(array[0].Trim());
-                numero2 = Convert.ToDouble(array[1].Trim());
-                Double resultado = numero1 * numero2;
-                _numbersOperate = resultado.ToString();
-                MessageBox.Show(_numbersOperate);
+            else if (OperationResult.Contains("*")){
+                array = OperationResult.Split('*');
+                PerformOperationMultiplication(array);
             }
-            else if (obj.Contains("/"))
-            {
-                array = obj.Split('/');
-                numero1 = Convert.ToDouble(array[0].Trim());
-                numero2 = Convert.ToDouble(array[1].Trim());
-                Double resultado = numero1 / numero2;
-                _numbersOperate = resultado.ToString();
-                MessageBox.Show(_numbersOperate);
+            else if (OperationResult.Contains("/")){
+                array = OperationResult.Split('/');
+                PerformOperationDivide(array);
             }
+        }
+        private static Double[] PerformOperationTrim(String[] array)
+        {
+            Double number1 = Convert.ToDouble(array[0].Trim());
+            Double number2 = Convert.ToDouble(array[1].Trim());
+            Double[] array2 = new Double[] { number1, number2 };
+            return array2;
+        }
+        private void PerformOperationSum(String[] array) {
+            Double[] array2 = PerformOperationTrim(array);
+            Double resultado = array2[0] + array2[1];
+            OperationResult = resultado.ToString();
+        }
+
+        private void PerformOperationDeduct(String[] array)
+        {
+            Double[] array2=PerformOperationTrim(array);
+            Double resultado = array2[0] - array2[1];
+            OperationResult = resultado.ToString();
+        }
+
+        private void PerformOperationMultiplication(String[] array)
+        {
+            Double[] array2 = PerformOperationTrim(array);
+            Double resultado = array2[0] * array2[1];
+            OperationResult = resultado.ToString();
+        }
+
+        private void PerformOperationDivide(String[] array)
+        {
+            Double[] array2 = PerformOperationTrim(array);
+            Double resultado = array2[0] / array2[1];
+            OperationResult = resultado.ToString();
         }
 
         private string _numbersOperate;
