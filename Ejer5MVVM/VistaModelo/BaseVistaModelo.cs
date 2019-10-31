003a931b9dcd82c67186697d810515944da868d5
Emmanuel <emmanuel@emmanuel.es>
2019-10-31 16:22:08 +0100

    Intento fallido de adaptacion del ejemplo de clase a mi calculadora.

---
+++ Ejer5MVVM/VistaModelo/BaseVistaModelo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace Ejer5MVVM.VistaModelo
+{
+    class BaseVistaModelo : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaiseProperty([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Ejer5MVVM/VistaModelo/VistaModelo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Ejer5MVVM.VistaModelo
+{
+    class VistaModelo : BaseVistaModelo
+    {
+        private RelayCommand _comprobarComando;
+        public ICommand comprobarComando => _comprobarComando;
+
+        private RelayCommand<int> _insertNumberCommand;
+        public ICommand InsertNumberCommand => _insertNumberCommand;
+
+        public void MainViewModel()
+        {
+            _comprobarComando = new RelayCommand(PerformCheckText);
+            _insertNumberCommand = new RelayCommand<int>(PerformInsertNumber);
+        }
+
+        private void PerformInsertNumber(int obj)
+        {
+            TextGreetings += obj.ToString();
+        }
+        private void PerformCheckText()
+        {
+            //aqui se puede hacer comprobaciones y ejecuciones
+            MessageBox.Show("Has pulsado 1");
+        }
+        private string _textGreetings;
+        public string TextGreetings
+        {
+            get => _textGreetings;
+            set
+            {
+                _textGreetings = value;
+                
+                RaiseProperty();
+            }
+        }
+    }
+}
