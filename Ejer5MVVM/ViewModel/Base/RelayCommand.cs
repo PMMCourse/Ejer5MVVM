d586f3280128264b3e4063ce830253bb7cae22a6
Fran <frasiecue@gmail.com>
2019-11-05 00:23:04 +0100

    Creacion de carpetas

---
+++ Ejer5MVVM/ViewModel/Base/RelayCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Ejer5MVVM.ViewModel.Base {
+    //Para la implementación de RelayCommand creamos dos implementaciones una con un parametro generico y otra sin él.
+    //Mientras que la implementación con generico, nos permitirá poder enviar un parametro y poder castearlo al tipo deseado
+    //La implementación sin comando estará más orientada a acciones (Comandos) que no requieran de parametro en su defecto.
+
+    public class RelayCommand : ICommand {
+        private Action _action;
+
+        public RelayCommand(Action action) {
+            _action = action;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) {
+            return true;
+        }
+
+        public void Execute(object parameter) {
+            _action?.Invoke();
+        }
+    }
+
+    public class RelayCommand<T> : ICommand {
+        public RelayCommand(Action<T> action) {
+            _action = action;
+        }
+
+        private Action<T> _action;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) {
+            return true;
+        }
+
+        public void Execute(object parameter) {
+            _action.Invoke((T)Convert.ChangeType(parameter, typeof(T)));
+        }
+    }
+}
---
+++ Ejer5MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Ejer5MVVM.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Ejer5MVVM.ViewModel {
+    //Heredamos el BaseViewModel con la intención de obtener todas las funcionalidades básicas para el ViewModel
+    public class MainViewModel : BaseViewModel {
+
+        //Cuando creamos un comando, creamos el campo privado de la implementación de ICommand, en este caso RelayCommand.
+        //Y la interfaz será publica, ya que es el mínimo exponente necesario para su uso.
+        private RelayCommand _checkTextGreetingsCommand;
+        public ICommand CheckTextGreetingsCommand => _checkTextGreetingsCommand;
+
+        private RelayCommand<int> _insertNumberCommand;
+        public ICommand InsertNumberCommand => _insertNumberCommand;
+
+        public MainViewModel() {
+            _checkTextGreetingsCommand = new RelayCommand(PerformCheckText);
+            _insertNumberCommand = new RelayCommand<int>(PerformInsertNumber);
+        }
+
+        private void PerformInsertNumber(int obj) {
+            TextGreetings += obj.ToString();
+        }
+
+        private void PerformCheckText() {
+            if (_textGreetings == "Hello World") {
+                MessageBox.Show("Bien!");
+            } else {
+                MessageBox.Show("Mal");
+            }
+        }
+
+        private string _textGreetings;
+
+        public string TextGreetings {
+            get => _textGreetings;
+            set {
+                _textGreetings = value;
+                //Cuando hacemos llamada del método RaiseProperty lanzamos la notificación de que esta 
+                //propiedad se ha visto modificada, para que todos los interesados puedan consultar su valor.
+                RaiseProperty();
+            }
+        }
+    }
+}
