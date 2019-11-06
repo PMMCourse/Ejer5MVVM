81b4500d7b6139c9c0203891b78b36b8231d787d
joaquinBB99 <joaquin.bejarano.brenes@alumnos.fesac.es>
2019-11-06 18:51:41 +0100

    Intento ejercico MVVM

---
+++ Ejer5MVVM/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer5MVVM.ViewModel.Base
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaiseProperty([CallerMemberName]string texto = "" )
+        {
+            PropertyChanged?.Invoke(this, PropertyChangedEventArgs(texto));
+        }
+        private PropertyChangedEventArgs PropertyChangedEventArgs(string texto)
+        {
+            throw new NotImplementedException();
+        }
+    } 
+}
---
+++ Ejer5MVVM/ViewModel/Base/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Ejer5MVVM.ViewModel.Base
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<int> introducirTexto;
+        private Action<string> mostrarTexto;
+
+        public RelayCommand(Action<int> introducirTexto)
+        {
+            this.introducirTexto = introducirTexto;
+        }
+
+        public RelayCommand(Action<string> mostrarTexto)
+        {
+            this.mostrarTexto = mostrarTexto;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
