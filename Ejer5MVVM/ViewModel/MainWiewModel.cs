bd63510188b5406e4f784cee0838bb0881f2fa88
Juan Carlos Cárdenas García <Jccarcia98@gmail.com>
2019-11-10 13:22:05 +0100

    .

---
+++ Ejer5MVVM/ViewModel/MainWiewModel.cs
@@ -0,0 +1,43 @@
+﻿using Ejer5MVVM.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer5MVVM.ViewModel
+{
+    public class MainWiewModel : BaseViewModel
+    {
+        List<string> terminos = new List<string>
+        {
+            "Pedro", "Maria", "Jose", "Juanki", "Chema"
+        };
+        
+        
+        private string _buscarPalabra;
+        public string buscarPalabra
+        {
+            get => _buscarPalabra;
+            set
+            {
+                _buscarPalabra = value;
+                encontrarPalabra = terminos.FirstOrDefault(x => x.ToLower().Contains(_buscarPalabra.ToLower()));
+                RaiseProperty();
+            }
+        }
+
+
+        
+        private string _encontrarPalabra;
+        public string encontrarPalabra
+        {
+            get => _encontrarPalabra;
+            set 
+            {
+                _encontrarPalabra = value;
+                RaiseProperty();
+            }
+        }
+    }
+}
