1b980b213f04c045f382bcd24bbc3d7d65d6d248
daniel.romero.rodriguez2@gmail.com <daniel.romero.rodriguez2@gmail.com>
2019-11-13 19:27:05 +0100

    Añadida la lógica del programa a servicios

---
+++ Ejer5MVVM/Services/CalcServices.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer5MVVM.Services
+{
+    public class CalcServices
+    {
+        String OperationResult;
+        public CalcServices(String OperationResult) {
+            this.OperationResult = OperationResult;
+        }
+        public string DoCalc() {
+            PerformInsertNumber();
+            return OperationResult;
+        }
+
+        public void PerformInsertNumber()
+        {
+            Double numero1, numero2;
+            String[] array = null;
+            if (OperationResult.Contains("+"))
+            {
+                array = OperationResult.Split('+');
+                PerformOperationSum(array);
+            }
+            else if (OperationResult.Contains("-"))
+            {
+                array = OperationResult.Split('-');
+                PerformOperationDeduct(array);
+            }
+            else if (OperationResult.Contains("*"))
+            {
+                array = OperationResult.Split('*');
+                PerformOperationMultiplication(array);
+            }
+            else if (OperationResult.Contains("/"))
+            {
+                array = OperationResult.Split('/');
+                PerformOperationDivide(array);
+            }
+        }
+        private static Double[] PerformOperationTrim(String[] array)
+        {
+            Double number1 = Convert.ToDouble(array[0].Trim());
+            Double number2 = Convert.ToDouble(array[1].Trim());
+            Double[] array2 = new Double[] { number1, number2 };
+            return array2;
+        }
+        private void PerformOperationSum(String[] array)
+        {
+            Double[] array2 = PerformOperationTrim(array);
+            Double resultado = array2[0] + array2[1];
+            OperationResult = resultado.ToString();
+        }
+
+        private void PerformOperationDeduct(String[] array)
+        {
+            Double[] array2 = PerformOperationTrim(array);
+            Double resultado = array2[0] - array2[1];
+            OperationResult = resultado.ToString();
+        }
+
+        private void PerformOperationMultiplication(String[] array)
+        {
+            Double[] array2 = PerformOperationTrim(array);
+            Double resultado = array2[0] * array2[1];
+            OperationResult = resultado.ToString();
+        }
+
+        private void PerformOperationDivide(String[] array)
+        {
+            Double[] array2 = PerformOperationTrim(array);
+            Double resultado = array2[0] / array2[1];
+            OperationResult = resultado.ToString();
+        }
+    }
+}
--- Ejer5MVVM/ViewModel/MainViewModel.cs
+++ Ejer5MVVM/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Ejer5MVVM.ViewModel.Base;
+﻿using Ejer5MVVM.Services;
+using Ejer5MVVM.ViewModel.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,65 +20,16 @@ namespace Ejer5MVVM.ViewModel
 
         public MainViewModel()
         {
-            _insertNumberCommand = new RelayCommand<String>(PerformInsertNumber);
+            _insertNumberCommand = new RelayCommand<String>(PerformOperate);
             _insertOperator = new RelayCommand<String>(PerformOperator);
         }
         private void PerformOperator(String obj)
         {
             OperationResult += obj;
         }
-        public void PerformInsertNumber(String obj)
-        {
-            Double numero1, numero2;
-            String[] array = null;
-            if (OperationResult.Contains("+")) {
-                array = OperationResult.Split('+');
-                PerformOperationSum(array);
-            } else if (OperationResult.Contains("-")) {
-                array = OperationResult.Split('-');
-                PerformOperationDeduct(array);
-            }
-            else if (OperationResult.Contains("*")){
-                array = OperationResult.Split('*');
-                PerformOperationMultiplication(array);
-            }
-            else if (OperationResult.Contains("/")){
-                array = OperationResult.Split('/');
-                PerformOperationDivide(array);
-            }
-        }
-        private static Double[] PerformOperationTrim(String[] array)
-        {
-            Double number1 = Convert.ToDouble(array[0].Trim());
-            Double number2 = Convert.ToDouble(array[1].Trim());
-            Double[] array2 = new Double[] { number1, number2 };
-            return array2;
-        }
-        private void PerformOperationSum(String[] array) {
-            Double[] array2 = PerformOperationTrim(array);
-            Double resultado = array2[0] + array2[1];
-            OperationResult = resultado.ToString();
-        }
 
-        private void PerformOperationDeduct(String[] array)
-        {
-            Double[] array2=PerformOperationTrim(array);
-            Double resultado = array2[0] - array2[1];
-            OperationResult = resultado.ToString();
-        }
-
-        private void PerformOperationMultiplication(String[] array)
-        {
-            Double[] array2 = PerformOperationTrim(array);
-            Double resultado = array2[0] * array2[1];
-            OperationResult = resultado.ToString();
-        }
-
-        private void PerformOperationDivide(String[] array)
-        {
-            Double[] array2 = PerformOperationTrim(array);
-            Double resultado = array2[0] / array2[1];
-            OperationResult = resultado.ToString();
+        public void PerformOperate(String obj) {
+           OperationResult=new CalcServices(OperationResult).DoCalc();
         }
 
         private string _numbersOperate;
