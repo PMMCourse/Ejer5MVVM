2262644bc753a173ea7ea65353aa2af1ea3c19ab
juankiduran12@gmail.com <juankiduran12@gmail.com>
2019-11-14 13:15:40 +0100

    EjercicioMVVM

---
+++ Ejer5MVVM/ViewModel/Base/RelayCommad.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EjerMVVM.ViewModel.Base
+{
+    public class RelayCommand<T> : ICommand
+    {
+        private Action<T> _action;
+
+        public RelayCommand(Action<T> action)
+        {
+            _action = action;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action?.Invoke((T)Convert.ChangeType(parameter, typeof(T)));
+        }
+    }
+}
+
